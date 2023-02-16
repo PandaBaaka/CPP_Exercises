efbe07cdb11e1d51b55cb86d0745b77b2088ce80
Agnetti Marc <agnettim.pro@gmail.com>
2023-02-16 14:49:58 +0100

    Test 01 passe

--- src/Card.hpp
+++ src/Card.hpp
@@ -1,1 +1,23 @@
 #pragma once
+
+#include <string>
+
+enum class CardType
+{
+    Monster,
+    Spell,
+    Trap
+};
+
+std::string to_string(CardType type)
+{
+    switch (type)
+    {
+    case CardType::Monster:
+        return "Monster";
+    case CardType::Spell:
+        return "Spell";
+    case CardType::Trap:
+        return "Trap";
+    }
+}
