385341448070da050e87d2cda7a3a1c5f79dbf02
Agnetti Marc <agnettim.pro@gmail.com>
2023-02-16 15:40:12 +0100

    Test 05 passe

--- src/Monster.hpp
+++ src/Monster.hpp
@@ -39,22 +39,28 @@ std::string to_symbol(Attribute attribute)
 class Monster : public Card
 {
 public:
-    Monster(std::string id, std::string name, Attribute attribute, std::string description, int atk, int def)
+    Monster(std::string id, std::string name, Attribute attribute, std::string monster_type, int atk, int def)
         : Card{id, CardType::Monster},
           _attribute{attribute},
+          _monster_type{monster_type},
           _atk{atk},
           _def{def}
     {
         set_name(name);
-        set_description(description);
     };
 
     Attribute get_attribute() const { return _attribute; }
     int get_atk() const { return _atk; }
     int get_def() const { return _def; }
+    std::string get_description() const
+    {
+        return "[" + _monster_type + "]\n" + Card::get_description() + "\nATK/" + std::to_string(_atk) + " DEF/" + std::to_string(_def);
+        ;
+    }
 
 private:
     Attribute _attribute = {};
+    std::string _monster_type;
     int _atk = 0;
     int _def = 0;
 };
