4df722a380a84e7daf4b709f7881ddba2ec1f1a4
Agnetti Marc <agnettim.pro@gmail.com>
2023-02-16 15:54:23 +0100

    Test 07 passe

--- src/Trap.hpp
+++ src/Trap.hpp
@@ -1,1 +1,41 @@
 #pragma once
+
+#include "Card.hpp"
+
+#include <string>
+
+enum class TrapType
+{
+    Normal,
+    Continuous,
+    Counter
+};
+
+std::string to_string(TrapType type)
+{
+    switch (type)
+    {
+    case TrapType::Normal:
+        return "Normal";
+    case TrapType::Continuous:
+        return "Continuous";
+    case TrapType::Counter:
+        return "Counter";
+    }
+}
+
+class Trap : public Card
+{
+public:
+    Trap(std::string id, std::string name, TrapType trap_type)
+        : Card(id, CardType::Trap),
+          _trap_type{trap_type}
+    {
+        set_name(name);
+    };
+
+    TrapType get_trap_type() const { return _trap_type; }
+
+private:
+    TrapType _trap_type = {};
+};
