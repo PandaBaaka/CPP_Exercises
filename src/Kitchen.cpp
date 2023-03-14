86fea4fd79d60f4ef016fb95873cb55c14a9b479
Agnetti Marc <agnettim.pro@gmail.com>
2023-03-14 15:39:33 +0100

    Test 02 passed

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -6,7 +6,7 @@ const Unit& Kitchen::register_unit(Unit unit)
     return _units.back();
 }
 
-const Unit* Kitchen::find_unit(const std::string& unit)
+const Unit* Kitchen::find_unit(const std::string& unit) const
 {
     // for (auto it = _units.begin(); it != _units.end(); ++it)
     // {
@@ -19,6 +19,29 @@ const Unit* Kitchen::find_unit(const std::string& unit)
 
     auto it = std::find_if(_units.begin(), _units.end(), [unit](const Unit& u) { return u.name == unit; });
     if (it != _units.end())
+    {
+        return &(*it);
+    }
+    return nullptr;
+}
+
+const Ingredient& Kitchen::register_ingredient(Ingredient ingredient)
+{
+    _ingredients.push_back(ingredient);
+    return _ingredients.back();
+}
+
+const Ingredient* Kitchen::find_ingredient(const std::string& ingredient) const
+{
+    auto it =
+        std::find_if(_ingredients.begin(), _ingredients.end(),
+                     [ingredient](const Ingredient& i)
+                     {
+                         return i.name.size() == ingredient.size() &&
+                                std::equal(i.name.begin(), i.name.end(), ingredient.begin(),
+                                           [](auto a, auto b) { return std::tolower(a) == std::tolower(b); });
+                     });
+    if (it != _ingredients.end())
     {
         return &(*it);
     }
--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../lib/Ingredient.hpp"
 #include "../lib/Unit.hpp"
 
 #include <algorithm>
@@ -10,8 +11,12 @@ class Kitchen
 {
 public:
     const Unit& register_unit(Unit unit);
-    const Unit* find_unit(const std::string& unit);
+    const Unit* find_unit(const std::string& unit) const;
+
+    const Ingredient& register_ingredient(Ingredient ingredient);
+    const Ingredient* find_ingredient(const std::string& ingredient) const;
 
 private:
-    std::list<Unit> _units;
+    std::list<Unit>       _units;
+    std::list<Ingredient> _ingredients;
 };
