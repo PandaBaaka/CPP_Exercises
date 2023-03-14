55942b240819d9ba72cfb09cf76580d8d48f70cd
Agnetti Marc <agnettim.pro@gmail.com>
2023-03-14 14:20:44 +0100

    kitchen start

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -1,1 +1,14 @@
 #include "Kitchen.hpp"
+
+const Unit& Kitchen::register_unit(Unit unit)
+{
+    auto it = _units.emplace(_units.end(), unit);
+    return *it;
+}
+
+const Unit* Kitchen::find_unit(const std::string& unit)
+{
+    // const auto it = std::find(_units.begin(), _units.end(), unit);
+    // return it != _units.end() ? &(*it) : nullptr;
+    return nullptr;
+}
--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,17 @@
 #pragma once
 
+#include "../lib/Unit.hpp"
+
+#include <algorithm>
+#include <list>
+#include <string>
+
 class Kitchen
-{};
+{
+public:
+    const Unit& register_unit(Unit unit);
+    const Unit* find_unit(const std::string& unit);
+
+private:
+    std::list<Unit> _units;
+};
