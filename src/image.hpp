e137a3dcfe845c2136f157250f30e7470f02daee
Agnetti Marc <agnettim.pro@gmail.com>
2023-03-28 13:48:57 +0200

    base setup

--- src/image.hpp
+++ src/image.hpp
@@ -1,13 +1,21 @@
 #pragma once
 
+#include "../lib/lib.hpp"
 #include "pixels.hpp"
 
-#include "../lib/lib.hpp"
-
-#include <stdint.h>
+#include <array>
 #include <cstddef>
 #include <functional>
-#include <string>
-#include <array>
 #include <iostream>
+#include <stdint.h>
+#include <string>
 
+template <typename P, size_t W, size_t H>
+class Image
+{
+public:
+    Image<P, W, H>(const std::function<P(const size_t i, const size_t j)>& functor)
+    {
+        // todo
+    }
+};
