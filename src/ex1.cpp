c1cf6ba0f419d573a1fad07bb5da1cbf5d58088c
Agnetti Marc <agnettim.pro@gmail.com>
2023-04-04 14:28:38 +0200

    first questions

--- src/ex1.cpp
+++ src/ex1.cpp
@@ -1,1 +1,21 @@
 #include "ex1.hpp"
+
+void pairwise_concatenate(std::list<std::list<int>>& list1, std::list<std::list<int>>& list2)
+{
+    auto it1 = list1.begin();
+    for (auto it2 = list2.begin(); it2 != list2.end(); ++it2)
+    {
+        concatenate(*it1, *it2);
+        *it1++;
+    }
+}
+
+void pairwise_concatenate(std::list<std::list<int>>& list1, std::list<std::list<int>>&& list2)
+{
+    auto it1 = list1.begin();
+    for (auto it2 = list2.begin(); it2 != list2.end(); ++it2)
+    {
+        concatenate(*it1, std::move(*it2));
+        *it1++;
+    }
+}
