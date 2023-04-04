18b5ef86a8e951741625c008454f8d86ab2a869b
Agnetti Marc <agnettim.pro@gmail.com>
2023-04-04 14:01:25 +0200

    done

--- src/image.hpp
+++ src/image.hpp
@@ -11,11 +11,53 @@
 #include <string>
 
 template <typename P, size_t W, size_t H>
-class Image
+struct Image
 {
 public:
-    Image<P, W, H>(const std::function<P(const size_t i, const size_t j)>& functor)
+    Image() = default;
+
+    Image(const P& pix)
     {
-        // todo
+        for (size_t j = 0; j < H; ++j)
+        {
+            for (size_t i = 0; i < W; ++i)
+            {
+                _pixels[j][i] = pix;
+            }
+        }
     }
+
+    Image(const std::function<P(const size_t, const size_t)>& functor)
+    {
+        for (size_t j = 0; j < H; ++j)
+        {
+            for (size_t i = 0; i < W; ++i)
+            {
+                (*this)(i, j) = functor(i, j);
+            }
+        }
+    }
+
+    P& operator()(const size_t i, const size_t j) { return _pixels[j][i]; }
+
+    const P& operator()(const size_t i, const size_t j) const { return _pixels[j][i]; }
+
+private:
+    std::array<std::array<P, W>, H> _pixels;
 };
+
+template <typename P, size_t W, size_t H>
+Image<P, W, H> operator+(const Image<P, W, H>& img1, const Image<P, W, H>& img2)
+{
+    Image<P, W, H> out = {};
+
+    for (unsigned long j = 0; j < H; ++j)
+    {
+        for (unsigned long i = 0; i < W; ++i)
+        {
+            out(i, j) = img1(i, j) + img2(i, j);
+        }
+    }
+
+    return out;
+}
--- src/pixels.hpp
+++ src/pixels.hpp
@@ -4,3 +4,63 @@
 
 #include <stdint.h>
 
+struct RGBA
+{
+    uint8_t r, g, b, a;
+};
+
+struct RGB
+{
+    uint8_t r, g, b;
+};
+
+struct Luma
+{
+    uint8_t gray;
+};
+
+template <typename A>
+A from(const A& x)
+{
+    return x;
+}
+
+template <typename A, typename B>
+A from(const B& stuff);
+
+template <>
+RGB from(const RGBA& x)
+{
+    return { x.r, x.g, x.b };
+}
+
+template <>
+RGBA from(const RGB& x)
+{
+    return { x.r, x.g, x.b, 255 };
+}
+
+template <>
+RGB from(const Luma& x)
+{
+    const auto [r, g, b] = lib::grayscale_to_rgb(x.gray);
+    return { r, g, b };
+}
+
+template <>
+Luma from(const RGB& x)
+{
+    return { lib::rgb_to_grayscale(x.r, x.g, x.b) };
+}
+
+template <>
+RGBA from(const Luma& x)
+{
+    return from<RGBA, RGB>(from<RGB, Luma>(x));
+}
+
+template <>
+Luma from(const RGBA& x)
+{
+    return from<Luma, RGB>(from<RGB, RGBA>(x));
+}
